b5cd7269e3534f2cd72f37548a186c5a2e14a373
Adam Szałański <adsn@softwaremind.com>
2023-12-27 18:36:46 +0100

    feat: non-blocking branch listing

--- src/main/java/com/example/github/repolister/githubcaller/response/UserRepoResponse.java
+++ src/main/java/com/example/github/repolister/githubcaller/response/UserRepoResponse.java
@@ -3,4 +3,5 @@ package com.example.github.repolister.githubcaller.response;
 import java.util.List;
 
 public record UserRepoResponse(String repositoryName, String ownerLogin, List<BranchResponse> branches) {
+
 }
